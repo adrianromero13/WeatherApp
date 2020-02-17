f660db50ef21c7175a3997ed36118ecb7a7a0ba6
Adrian Romero <radrian031613@gmail.com>
2020-02-16 18:46:32 -0800

    added html elements to house the content received from the weather api. The content will be housed in ptags with id's corresponding to the content pulled.

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 // weather app
 // const weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?";
 // const key = "id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
+// const exampleURL = "api.openweathermap.org/data/2.5/weather?q={city name},{state}&appid={your api key}"
 
-let weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
+let weatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" +  `${city,state}` + "id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
 
-/* -----------------guidelines------------------
-* Use AJAX to hook into the API to retrieve data in JSON format.
+//  -----------------guidelines------------------
+// * Use AJAX to hook into the API to retrieve data in JSON format.
+  // create URL based on user input
+  // 1. call html elements
+let 
 
-* Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+// * Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-* Display the following under current weather conditions:
+// * Display the following under current weather conditions:
 
-  * City
+//   * City
 
-  * Date
+//   * Date
 
-  * Icon image (visual representation of weather conditions)
+//   * Icon image (visual representation of weather conditions)
 
-  * Temperature
+//   * Temperature
+
+//   * Humidity
+
+//   * Wind speed
+
+//   * UV index
+
+// * Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city. 
+
+// * Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
+
+//   * Date
+
+//   * Icon image (visual representation of weather conditions)
+
+//   * Temperature
 
   * Humidity
-
-  * Wind speed
-
-  * UV index
-
-* Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city. 
-
-* Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
-
-  * Date
-
-  * Icon image (visual representation of weather conditions)
-
-  * Temperature
-
-  * Humidity
-  -------------------------------*/
+  // -------------------------------

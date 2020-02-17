ced5fb4d317dd2ef6e48bed95930c000542e1cfc
Adrian Romero <radrian031613@gmail.com>
2020-02-16 17:00:06 -0800

    Created repository with a script.js file an index.html file and an asset folder including a style.css file. Added code to the html with Bulma CDN and designed a header section for the webpage and a search input for the user.

---
+++ script.js
@@ -0,0 +1,39 @@
+// weather app
+// const weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?";
+// const key = "id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
+
+let weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
+
+/* -----------------guidelines------------------
+* Use AJAX to hook into the API to retrieve data in JSON format.
+
+* Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+* Display the following under current weather conditions:
+
+  * City
+
+  * Date
+
+  * Icon image (visual representation of weather conditions)
+
+  * Temperature
+
+  * Humidity
+
+  * Wind speed
+
+  * UV index
+
+* Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city. 
+
+* Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
+
+  * Date
+
+  * Icon image (visual representation of weather conditions)
+
+  * Temperature
+
+  * Humidity
+  -------------------------------*/

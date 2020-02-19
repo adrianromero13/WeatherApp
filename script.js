86c5d7d1d8685d429d4fedbe799ec48c6c68bb7b
Adrian Romero <radrian031613@gmail.com>
2020-02-18 18:47:11 -0800

    Set up API from weathermap for receiving data about the city the user inputs. Set up jquery in the scripts file to change the HTML text to show the information received from the API.

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-// weather app
-// const weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?";
-// const key = "id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
-// const exampleURL = "api.openweathermap.org/data/2.5/weather?q={city name},{state}&appid={your api key}"
+$('#searchCity').on('click',function(event){
+  event.preventDefault()
+  let searchText = $('#locationText').val()
+  let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=a126d7e96a0d4ee2f269cafd5b34ca51&units=imperial`
+  
+  $.ajax({
+      type:'GET',
+      url:url
+  })
+  .then(function(data){
+      console.log(data);
+    // // grab id currentCity
+    // // append data
+    $('#currentCity').text(data.name);
+    console.log(data.name);
+    // grab id cityTemp
+    $('#cityTemp').text(Math.floor(data.main.temp) + "°F");
+    // append data
+    $('#cityHumidity').text((data.main.humidity) + "%");
+    $('#cityWind').text((data.wind.speed) + " MPH");
+    $('#weatherImage').attr('src', data.weather[0].icon);
+    console.log(data.weather.icon);
+  
 
-let weatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" +  `${city,state}` + "id=524901&APPID=a126d7e96a0d4ee2f269cafd5b34ca51";
 
-//  -----------------guidelines------------------
-// * Use AJAX to hook into the API to retrieve data in JSON format.
-  // create URL based on user input
-  // 1. call html elements
-let 
+      // $('#currentCity').text(`
+      // <h3>${Date()}</h3>
+      // <h2>${data.name}</h2>
+      // <p>${Math.floor(data.main.temp)}, ${Math.floor(data.main.temp_min)}, ${Math.floor(data.main.temp_max)}</p>
+      // <p>${data.main.humidity} HUMIDITY</p>
+      // <p>${data.wind.speed} WIND SPEED</p>
+  
+      // `)
+    // local storage
+    let local = localStorage.getItem('city')
+    
+    // inside a function localStorage.setItem('city', value)
+    // append local the variable
+  });
 
-// * Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-// * Display the following under current weather conditions:
-
-//   * City
-
-//   * Date
-
-//   * Icon image (visual representation of weather conditions)
-
-//   * Temperature
-
-//   * Humidity
-
-//   * Wind speed
-
-//   * UV index
-
-// * Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city. 
-
-// * Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
-
-//   * Date
-
-//   * Icon image (visual representation of weather conditions)
-
-//   * Temperature
-
-  * Humidity
-  // -------------------------------
+  // let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchText}&APPID=a126d7e96a0d4ee2f269cafd5b34ca51&units=imperial`
+  // $.ajax({
+  //     type:'GET',
+  //     url:queryUrl
+  // })
+  // .then(function(data){
+  //     console.log(data)
+  //     for (let i = 0; i < data.list.length; i=i+8){
+  //         $('#forecast').append(
+  //             `<div class = 'cards'>
+  //             <h3>${data.list[i].dt_txt.split(' ')[0]}</h3>
+  //             <p>${Math.floor(data.list[i].main.temp)}°F</p>
+  //             <img src='http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png'/>
+  //             <p>${data.list[i].main.humidity}%</p>
+              
+  
+  //             </div>
+  //             `
+  //         )
+  //     }
+  // })
+  })

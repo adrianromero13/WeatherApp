81050b1e97527222532bc40e3d64677a953d4303
Adrian Romero <radrian031613@gmail.com>
2020-02-19 18:35:36 -0800

    set up jquery to append searched cities into a div as buttons with the text as the searched city. Set up a variable to create an array of searched cities and stored into local storage.

--- script.js
+++ script.js
@@ -1,63 +1,114 @@
+
+
+
+let baseURL = 'https://api.openweathermap.org/data/2.5/';
+let apiKey = '&appid=a126d7e96a0d4ee2f269cafd5b34ca51';
+let url;
+let searchCity = $('#searchCity');
+let cities = [];
+let listDiv = $('#prevCities');
+// function to set up ajax calls to API on click of search button
 $('#searchCity').on('click',function(event){
   event.preventDefault()
   let searchText = $('#locationText').val()
-  let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=a126d7e96a0d4ee2f269cafd5b34ca51&units=imperial`
-  
+  url=`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=a126d7e96a0d4ee2f269cafd5b34ca51&units=imperial`
+
+
+  //ajax call for the UVIndex element
   $.ajax({
       type:'GET',
       url:url
   })
   .then(function(data){
       console.log(data);
-    // // grab id currentCity
-    // // append data
     $('#currentCity').text(data.name);
     console.log(data.name);
     // grab id cityTemp
-    $('#cityTemp').text(Math.floor(data.main.temp) + "°F");
+    $('#cityTemp').text("Temperature: " + (Math.floor(data.main.temp)) + "°F");
     // append data
-    $('#cityHumidity').text((data.main.humidity) + "%");
-    $('#cityWind').text((data.wind.speed) + " MPH");
+    $('#cityHumidity').text("Humidity: " + (data.main.humidity) + "%");
+    $('#cityWind').text("Wind Speed: " + (data.wind.speed) + " MPH");
     $('#weatherImage').attr('src', data.weather[0].icon);
-    console.log(data.weather.icon);
   
-
-
-      // $('#currentCity').text(`
-      // <h3>${Date()}</h3>
-      // <h2>${data.name}</h2>
-      // <p>${Math.floor(data.main.temp)}, ${Math.floor(data.main.temp_min)}, ${Math.floor(data.main.temp_max)}</p>
-      // <p>${data.main.humidity} HUMIDITY</p>
-      // <p>${data.wind.speed} WIND SPEED</p>
-  
-      // `)
-    // local storage
-    let local = localStorage.getItem('city')
+    saveData(data)
+    function getuvindex(lat, lon) {
+        queryURL = baseURL + "uvi?" + apiKey + "&lat=" + lat + "&lon=" + lon;
     
-    // inside a function localStorage.setItem('city', value)
-    // append local the variable
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+    
+                let pUVI = $("<p>").html("<b>UV Index : <b>");
+                let spanUVI = $("<span>").html("<b>" + response.value + "</b>");
+                spanUVI.attr("class", "uvindex");
+                pUVI.append(spanUVI);
+                $(".currentcity").append(pUVI);
+            });
+    
+    }return getuvindex;
+
   });
 
 
-  // let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchText}&APPID=a126d7e96a0d4ee2f269cafd5b34ca51&units=imperial`
-  // $.ajax({
-  //     type:'GET',
-  //     url:queryUrl
-  // })
-  // .then(function(data){
-  //     console.log(data)
-  //     for (let i = 0; i < data.list.length; i=i+8){
-  //         $('#forecast').append(
-  //             `<div class = 'cards'>
-  //             <h3>${data.list[i].dt_txt.split(' ')[0]}</h3>
-  //             <p>${Math.floor(data.list[i].main.temp)}°F</p>
-  //             <img src='http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png'/>
-  //             <p>${data.list[i].main.humidity}%</p>
+  function saveData(data) {
+    console.log(url);
+    
+    let inputSearchCity = data.name
+    if (inputSearchCity === "") {
+        return;
+    }
+    // seperates the cities based on the "space"
+    inputSearchCity = inputSearchCity.split(" ");
+    searchCity = "";
+
+    //Capitalizing the first letters of cities' each word
+    inputSearchCity.forEach(element => {
+        searchCity += " " + element.charAt(0).toUpperCase() + element.slice(1);
+    });
+
+    //prevents to add an already existing citi to the list as well as to localstorage
+    
+    
+    if (cities.includes(searchCity)) {
+        $("#locationText").val("");
+        return;
+    }
+    cities.push(searchCity);
+    localStorage.setItem("cities", JSON.stringify(cities));
+
+    //displaying the searched citi names on the left pane
+    listTag = $("<button>").html("<b>" + searchCity + "</b>");
+    listTag.attr("data-city", searchCity);
+    listTag.addClass("list-group-item list-group-item-action list-group-item-primary citybutton");
+    listDiv.append(listTag);
+    $("#locationText").val("");
+    rendercity(searchCity);
+    // console.log(searchCity);
+
+};
+
+
+  let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchText}&APPID=a126d7e96a0d4ee2f269cafd5b34ca51&units=imperial`
+  $.ajax({
+      type:'GET',
+      url:queryUrl
+  })
+  .then(function(data){
+      console.log(data)
+      for (let i = 0; i < data.list.length; i=i+8){
+          $('#forecast').append(
+              `<div class = 'cards'>
+              <h3>${data.list[i].dt_txt.split(' ')[0]}</h3>
+              <p>${Math.floor(data.list[i].main.temp)}°F</p>
+              <img src='http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png'/>
+              <p>${data.list[i].main.humidity}%</p>
               
   
-  //             </div>
-  //             `
-  //         )
-  //     }
-  // })
+              </div>
+              `
+          )
+      }
+  })
   })
